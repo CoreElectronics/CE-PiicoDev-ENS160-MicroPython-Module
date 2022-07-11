1835e5cb6e089ab7e1e08428c296fc7922ff0bee
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-07-11 13:25:17 +1000

    Update to latest PiicoDev standards

--- PiicoDev_ENS160.py
+++ PiicoDev_ENS160.py
@@ -42,26 +42,26 @@ _VAL_OPMODE_IDLE       = 0x01
 _VAL_OPMODE_STANDARD   = 0x02
 _VAL_OPMODE_RESET      = 0xF0
 
-def _readBit(x, n):
+def _read_bit(x, n):
     return x & 1 << n != 0
 
-def _readCrumb(x, n):
-    return _readBit(x, n) + _readBit(x, (n+1))^2
+def _read_crumb(x, n):
+    return _read_bit(x, n) + _read_bit(x, (n+1))^2
 
-def _readTribit(x, n):
-    return _readBit(x, n) + _readBit(x, (n+1))^2 + _readBit(x, (n+2))^3
+def _read_tribit(x, n):
+    return _read_bit(x, n) + _read_bit(x, (n+1))^2 + _read_bit(x, (n+2))^3
 
-def _setBit(x, n):
+def _set_bit(x, n):
     return x | (1 << n)
 
-def _clearBit(x, n):
+def _clear_bit(x, n):
     return x & ~(1 << n)
 
-def _writeBit(x, n, b):
+def _write_bit(x, n, b):
     if b == 0:
-        return _clearBit(x, n)
+        return _clear_bit(x, n)
     else:
-        return _setBit(x, n)
+        return _set_bit(x, n)
 
 class PiicoDev_ENS160(object):
     def __init__(self, bus=None, freq=None, sda=None, scl=None, address=_I2C_ADDRESS, intdat=False, intgpr=False, int_cfg=0, intpol=0, temperature=25, humidity=70):
@@ -76,26 +76,26 @@ class PiicoDev_ENS160(object):
         self.address = address
         config = 0x00
         if intdat or intgpr:
-            config = _setBit(config, _BIT_CONFIG_INTEN)
-            config = _writeBit(config, _BIT_CONFIG_INTDAT, intdat)
-            config = _writeBit(config, _BIT_CONFIG_INTGPR, intgpr)
-        config = _writeBit(config, _BIT_CONFIG_INT_CFG, int_cfg)
-        config = _writeBit(config, _BIT_CONFIG_INTPOL, intpol)
+            config = _set_bit(config, _BIT_CONFIG_INTEN)
+            config = _write_bit(config, _BIT_CONFIG_INTDAT, intdat)
+            config = _write_bit(config, _BIT_CONFIG_INTGPR, intgpr)
+        config = _write_bit(config, _BIT_CONFIG_INT_CFG, int_cfg)
+        config = _write_bit(config, _BIT_CONFIG_INTPOL, intpol)
         self.config = config
         try:
-            part_id = self._readInt(_REG_PART_ID, 2)
+            part_id = self._read_int(_REG_PART_ID, 2)
             print('part_id: ' + str(part_id))
             if part_id != _VAL_PART_ID:
                 print('Device is not PiicoDev ENS160')
                 raise SystemExit
-            self._writeInt(_REG_OPMODE, _VAL_OPMODE_STANDARD, 1)
+            self._write_int(_REG_OPMODE, _VAL_OPMODE_STANDARD, 1)
             #self._write(_REG_OPMODE, bytes(_VAL_OPMODE_STANDARD))
             sleep_ms(20)
             print('written opmode standard')
-            opmode = self._readInt(_REG_OPMODE, 1)
-            print('op mode: ' + str(opmode))
+            opmode = self._read_int(_REG_OPMODE, 1)
+            print('OPMODE: ' + str(opmode))
             sleep_ms(20)
-            self._writeInt(_REG_CONFIG, self.config, 1)
+            self._write_int(_REG_CONFIG, self.config, 1)
             print('written config register')
             self.setTemperature(temperature)
             print('set the temperature')
@@ -119,20 +119,20 @@ class PiicoDev_ENS160(object):
             print(i2c_err_str.format(self.address))
             return None
         
-    def _readInt(self, register, length):
+    def _read_int(self, register, length):
         return int.from_bytes(self._read(register, length),'little')
 
-    def _writeInt(self, register, integer, length):
+    def _write_int(self, register, integer, length):
         return self._write(register, int.to_bytes(integer,length,'little'))
         
     def getDeviceStatus(self):
         device_status = int.from_bytes(self._read(_REG_DEVICE_STATUS, 1),'little')
         print('device_status: ' + str(device_status))
-        self.statas = _readBit(device_status, _BIT_DEVICE_STATUS_STATAS)
-        self.stater = _readBit(device_status, _BIT_DEVICE_STATUS_STATER)
-        self.validity_flag = _readCrumb(device_status, _BIT_DEVICE_STATUS_VALIDITY_FLAG)
-        self.newdat = _readBit(device_status, _BIT_DEVICE_STATUS_NEWDAT)
-        self.newgpr = _readBit(device_status, _BIT_DEVICE_STATUS_NEWGPR)
+        self.statas = _read_bit(device_status, _BIT_DEVICE_STATUS_STATAS)
+        self.stater = _read_bit(device_status, _BIT_DEVICE_STATUS_STATER)
+        self.validity_flag = _read_crumb(device_status, _BIT_DEVICE_STATUS_VALIDITY_FLAG)
+        self.newdat = _read_bit(device_status, _BIT_DEVICE_STATUS_NEWDAT)
+        self.newgpr = _read_bit(device_status, _BIT_DEVICE_STATUS_NEWGPR)
         
     def setTemperature(self, temperature):
         kelvin = int(temperature + 273.15)
@@ -140,38 +140,41 @@ class PiicoDev_ENS160(object):
         buf[0] = (kelvin * 64) & 0xFF
         buf[1] = ((kelvin *64) & 0xFF00) >> 8
         #self._write(_REG_TEMP_IN, bytes(buf))
-        self._writeInt(_REG_TEMP_IN, kelvin * 64, 2)
+        self._write_int(_REG_TEMP_IN, kelvin * 64, 2)
             
     def setHumidity(self, humidity):
         buf = [0,0]
         buf[0] = ((humidity * 512) & 0xFF)
         buf[1] = ((humidity * 512) & 0xFF00) >> 8
 #        self._write(_REG_RH_IN, bytes(buf))
-        self._writeInt(_REG_RH_IN, humidity * 512, 2)
+        self._write_int(_REG_RH_IN, humidity * 512, 2)
         
     def getTemperature(self):
-        kelvin = self._readInt(_REG_DATA_T, 2) / 64
+        kelvin = self._read_int(_REG_DATA_T, 2) / 64
         return kelvin - 273.15
         
     def getHumidity(self):
-        return self._readInt(_REG_DATA_RH, 2) / 512
+        return self._read_int(_REG_DATA_RH, 2) / 512
     
-    def readAQI(self):
-        aqi = _readTribit(self._readInt(_REG_DATA_AQI, 2), 0)
+    @property
+    def aqi(self):
+        aqi = _read_tribit(self._read_int(_REG_DATA_AQI, 2), 0)
         if aqi is not None:
             return aqi
         else:
             return aqi
-    
-    def readTVOC(self):
-        tvoc = self._readInt(_REG_DATA_TVOC, 2)
+
+    @property
+    def tvoc(self):
+        tvoc = self._read_int(_REG_DATA_TVOC, 2)
         if tvoc is not None:
             return tvoc
         else:
             return tvoc
-        
-    def readECO2(self):
-        eco2 = self._readInt(_REG_DATA_ECO2, 2)
+    
+    @property
+    def eco2(self):
+        eco2 = self._read_int(_REG_DATA_ECO2, 2)
         if eco2 is not None:
             return eco2
         else:
